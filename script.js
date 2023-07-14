215bbf10de145590e1950739de0ae214eb2c7bd7
shubham07rana <134138285+shubham07rana@users.noreply.github.com>
2023-07-14 23:49:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 function fibonacci(num) {
-// your code here
+// your code herefunction fibonacci(num) {
+  if (num === 0) {
+    return 0;
+  } else if (num === 1) {
+    return 1;
+  } else {
+    return fibonacci(num - 1) + fibonacci(num - 2);
+  }
+}
 }
 
 module.exports = fibonacci;
